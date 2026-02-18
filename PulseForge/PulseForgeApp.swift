2c3e7eea02c2082c2edcca899e8c8b73a624173d
Rangerboy574 <jdeweese574@icloud.com>
2026-02-18 11:37:34 -0600

    Initial Commit

---
+++ PulseForge/PulseForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseForgeApp.swift
+//  PulseForge
+//
+//  Created by Joseph DeWeese on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
